c9370d17c9b779261b625cc8eba6e998f1d47a24
Aurelia <aurelaurelzr@gmail.com>
2026-04-19 12:30:01 +0700

    Refactor code structure for improved readability and maintainability

--- lib/features/home/widgets/featured_card.dart
+++ lib/features/home/widgets/featured_card.dart
@@ -21,9 +21,95 @@ class FeaturedCard extends StatelessWidget {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(24),
               ),
-              child: Padding(padding: const EdgeInsets.all(4.0)),
-            )
-            ,
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Column(
+                  children: [
+                    //Image articles
+                    Stack(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          height: 196,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                              image: AssetImage("assets/images/artikel1.jpg"),
+                              fit: BoxFit.cover,
+                              alignment: Alignment.topCenter, 
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 10,
+                          left: 10,
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 8,
+                              vertical: 4,
+                            ),
+                            decoration: BoxDecoration(
+                              color: AppColors.primary,
+                              borderRadius: BorderRadius.circular(99),
+                            ),
+                            child: Text(
+                              'Healthy Tips',
+                              style: TextStyle(
+                                color: AppColors.secondary,
+                                fontSize: 10,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    //Article title
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  'Trending Article',
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    color: AppColors.hintText,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                Text(
+                                  '3 days ago',
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    color: AppColors.hintText,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                )
+                              ],
+                            ),
+                            Text(
+                              'The benefits of sleeping on time is blablablableblebleblublublu',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 13,
+                                fontWeight: FontWeight.w600,
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           );
         },
       ),
