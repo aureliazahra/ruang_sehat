72e6181006d3e0c3f9641ab405f44f4f1ee13f7f
Aurelia <aurelaurelzr@gmail.com>
2026-04-22 07:25:25 +0700

    feat: update FeaturedCard and RecommendedCard to display article details dynamically

--- lib/features/home/widgets/featured_card.dart
+++ lib/features/home/widgets/featured_card.dart
@@ -11,128 +11,129 @@ class FeaturedCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-  height: 300,
-  child: Consumer<ArticleProviders>(
-    builder: (context, provider, child) {
-      if (provider.isLoading) {
-        return const Center(child: CircularProgressIndicator());
-      }
+      height: 300,
+      child: Consumer<ArticleProviders>(
+        builder: (context, provider, child) {
+          if (provider.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-      if (provider.errorMessage != null) {
-        return Center(child: Text(provider.errorMessage!));
-      }
+          if (provider.errorMessage != null) {
+            return Center(child: Text(provider.errorMessage!));
+          }
 
-      return ListView.separated(
-        scrollDirection: Axis.horizontal,
-        itemCount: provider.articles.length,
-        separatorBuilder: (_, __) => const SizedBox(width: 8),
-        itemBuilder: (context, index) {
-          final article = provider.articles[index];
+          return ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemCount: provider.articles.length,
+            separatorBuilder: (_, __) => const SizedBox(width: 8),
+            itemBuilder: (context, index) {
+              final article = provider.articles[index];
 
-                return SizedBox(
-                  width: 200,
-                  child: Card(
-                    color: AppColors.secondary,
-                    clipBehavior: Clip.antiAlias,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Column(
-                        children: [
-                          //Image articles
-                          Stack(
-                            children: [
-                              Container(
-                                width: double.infinity,
-                                height: 196,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  image: DecorationImage(
-                                    image: NetworkImage("$baseUrl/${article.image}"),
-                                    fit: BoxFit.cover,
-                                    alignment: Alignment.topCenter,
+              return SizedBox(
+                width: 200,
+                child: Card(
+                  color: AppColors.secondary,
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Column(
+                      children: [
+                        //Image articles
+                        Stack(
+                          children: [
+                            Container(
+                              width: double.infinity,
+                              height: 196,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    "$baseUrl/${article.image}",
                                   ),
+                                  fit: BoxFit.cover,
+                                  alignment: Alignment.topCenter,
                                 ),
-                              ),
-                              Positioned(
-                                top: 10,
-                                left: 10,
-                                child: Container(
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: 8,
-                                    vertical: 4,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    color: AppColors.primary,
-                                    borderRadius: BorderRadius.circular(99),
-                                  ),
-                                  child: Text(
-                                    'Healthy Tips',
-                                    style: TextStyle(
-                                      color: AppColors.secondary,
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          //Article title
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.all(12.0),
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      const Text(
-                                        'Trending Article',
-                                        style: TextStyle(
-                                          fontSize: 10,
-                                          color: AppColors.hintText,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      ),
-                                      Text(
-                                        '3 days ago',
-                                        style: TextStyle(
-                                          fontSize: 10,
-                                          color: AppColors.hintText,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  Text(
-                                    'The benefits of sleeping on time is blablablableblebleblublublu',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                    maxLines: 2,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ],
                               ),
                             ),
+                            Positioned(
+                              top: 10,
+                              left: 10,
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 8,
+                                  vertical: 4,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: AppColors.primary,
+                                  borderRadius: BorderRadius.circular(99),
+                                ),
+                                child: Text(
+                                  article.category,
+                                  style: TextStyle(
+                                    color: AppColors.secondary,
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        //Article title
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                     Text(
+                                      'Trending Article no ${index + 1}',
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        color: AppColors.hintText,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    Text(
+                                      article.date,
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        color: AppColors.hintText,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  article.title,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                );
-              },
-            );
-          },
-        ),
-      );
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
--- lib/features/home/widgets/recommended_card.dart
+++ lib/features/home/widgets/recommended_card.dart
@@ -1,97 +1,113 @@
 import 'package:flutter/material.dart';
 import 'package:ruang_sehat/theme/app_colors.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:ruang_sehat/features/articles/providers/articles_providers.dart';
 
 class RecommendedCard extends StatelessWidget {
   const RecommendedCard({super.key});
+  static final String baseUrl = dotenv.env['BASE_URL']!;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      physics: const NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      separatorBuilder: (_, __) => const SizedBox(height: 7),
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return Card(
-          color: AppColors.secondary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(24),
-          ),
-          child: Padding(
-            padding: EdgeInsetsGeometry.all(12),
-            child: Row(
-              children: [
-                //Image articles
-                Container(
-                  width: 75,
-                  height: 75,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    image: DecorationImage(
-                      image: AssetImage("assets/images/artikel1.jpg"),
-                      fit: BoxFit.cover,
-                      alignment: Alignment.topCenter,
+    return Consumer<ArticleProviders>(
+      builder: (context, provider, _) {
+        if (provider.isLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (provider.articles.isEmpty) {
+          return const Center(child: Text("Tidak ada artikel"));
+        }
+        return ListView.separated(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          separatorBuilder: (_, __) => const SizedBox(height: 7),
+          itemCount: provider.articles.length,
+          itemBuilder: (context, index) {
+            final article = provider.articles[index];
+            return Card(
+              color: AppColors.secondary,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24),
+              ),
+              child: Padding(
+                padding: EdgeInsetsGeometry.all(12),
+                child: Row(
+                  children: [
+                    //Image articles
+                    Container(
+                      width: 75,
+                      height: 75,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        image: DecorationImage(
+                          image: NetworkImage("$baseUrl/${article.image}"),
+                          fit: BoxFit.cover,
+                          alignment: Alignment.topCenter,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                // Article title
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(7.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // Article title
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(7.0),
+                        child: Column(
                           children: [
-                            Container(
-                              padding: EdgeInsets.symmetric(
-                                horizontal: 8,
-                                vertical: 2,
-                              ),
-                              decoration: BoxDecoration(
-                                color: AppColors.primary.withOpacity(0.3),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(99),
-                                ),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  'Healthy Tips',
-                                  style: TextStyle(
-                                    color: AppColors.primary,
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w600,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 8,
+                                    vertical: 2,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    color: AppColors.primary.withOpacity(0.3),
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(99),
+                                    ),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      article.category,
+                                      style: TextStyle(
+                                        color: AppColors.primary,
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
+                                Text(
+                                  article.date,
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: AppColors.hintText,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
                             ),
                             Text(
-                              '2026-01-27',
+                              article.title,
                               style: TextStyle(
-                                fontSize: 12,
-                                color: AppColors.hintText,
-                                fontWeight: FontWeight.w500,
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
                               ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
                             ),
                           ],
                         ),
-                        Text(
-                          "The benefit of sleeping on time and how to fix it",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         );
       },
     );
